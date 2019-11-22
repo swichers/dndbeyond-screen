d73a60d0cc581fe3020df965c45754badf948493
Steven Wichers <me@stevenwichers.com>
2019-11-21 17:07:54 -0800

    Slight refactoring and add comments.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,13 +7,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+// @TODO: Replace jquery with jquery-slim.
 const $ = require('jquery');
 
 $(document).ready(() => {
+  // Init the tooltips we have for various properties.
   $('body').tooltip({
     container: 'body',
     selector: '[data-toggle="tooltip"]',
   });
+  // Init the popovers we use to show spell information.
   $('body').popover({
     container: 'body',
     selector: '[data-toggle="popover"]',
@@ -21,10 +24,9 @@ $(document).ready(() => {
   });
 
   let character_updaters = {};
+  // Start the automatic refreshing for all available characters.
   document.querySelectorAll('.character[data-character-id]').forEach((card) => {
     let character_id = card.getAttribute('data-character-id');
     character_updaters[character_id] = new CharacterUpdater(character_id);
   });
 });
-
-
